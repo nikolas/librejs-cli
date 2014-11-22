a4f43bebada34252873be065db330aa417a01abb
Nik Nyby <nikolas@gnu.org>
2014-11-22 04:01:59 -0500

    new isTrivial test

--- tests/unit/librejs-analyzer/script/test-script.js
+++ tests/unit/librejs-analyzer/script/test-script.js
@@ -21,7 +21,7 @@ test('Test the Script constructor', function() {
     ok(scriptConstructorWithNoData);
 });
 
-test('Test _checkForTriviality', function() {
+test('Test _isTrivial false with function', function() {
     var report = this.script.analyze();
     var item;
     for (var i = 0; i < report.length; i++) {
@@ -31,6 +31,20 @@ test('Test _checkForTriviality', function() {
         }
     }
     strictEqual(item.val, false);
+});
+
+test('Test _isTrivial true', function() {
+    var js = 'console.log(1 + 2);';
+    var script = new Script({data: js});
+    var report = script.analyze();
+    var item;
+    for (var i = 0; i < report.length; i++) {
+        if (report[i].type === 'triviality') {
+            item = report[i];
+            break;
+        }
+    }
+    strictEqual(item.val, true);
 });
 
 test('Test _findLicStartLicense', function() {
