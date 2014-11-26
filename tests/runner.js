a3c82eb66ced9f992528aee9133fed46e8b1aaaa
Nik Nyby <niknyby@riseup.net>
2014-11-26 18:30:59 -0500

    license notice in test files

--- tests/runner.js
+++ tests/runner.js
@@ -1,3 +1,23 @@
+/*
+ * librejs-cli - A command-line tool for GNU LibreJS
+ * Copyright (C) 2014 Nik Nyby
+ *
+ * This file is part of librejs-cli.
+ *
+ * librejs-cli is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * librejs-cli is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with librejs-cli.  If not, see  <http://www.gnu.org/licenses/>.
+ */
+
 'use strict';
 
 var testrunner = require('qunit');
